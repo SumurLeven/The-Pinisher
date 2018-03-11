cb11444cfdee069c3d0aab4ee3f89e56314af10c
SumurLeven <37259362+SumurLeven@users.noreply.github.com>
2018-03-11 11:31:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    consumer_key: "",
-    consumer_secret: "",
-    token: "",
-    token_secret: ""
+    consumer_key: "8efxuDtYuQK5NGmbygf2mxkWj",
+    consumer_secret: "	V74U5iHuNx0Jzc1oD1TCQvvV2ot7b9E15CK2nFmL3CEXg8oWK8",
+    token: "861810447774765056-UDyhxf0p0AnKUuC17zGtL7ADzJYxfwE",
+    token_secret: "9AB1vI4vl2LIGLp98x4CtXo19QkTIXVptnR4ekpzf7PYY"
 
 }
